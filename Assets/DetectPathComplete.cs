b9daa6663920e704774e07c4d54c30ba0d5a66da
Teddy <richardgeorgeoff@gmail.com>
2025-01-20 00:21:51 -0800

    Fix Pathing Action Subscription

--- Assets/DetectPathComplete.cs
+++ Assets/DetectPathComplete.cs
@@ -1,31 +1,32 @@
+using System.Collections;
 using System.Collections.Generic;
 using Pathfinding;
 using UnityEngine;
 
 public class DetectPathComplete : MonoBehaviour
 {
-    public Seeker Seeker;
     public AIDestinationSetter DestinationSetter;
     public PlayMakerFSM FSMToSendEventTo;
-
+    public AIPath path;
     public List<Transform> Locations = new ();
     public int currentLoc;
+    public float pauseDurationAfterReachDestination = 1;
     
     // Start is called before the first frame update
     void Start()
     {
-        Seeker.pathCallback += PathCompletedSendFSMEvent;
+        path.OnTargetReachedActions.Add(PathCompletedSendFSMEvent);
     }
 
-    private void PathCompletedSendFSMEvent(Path p)
+    private void PathCompletedSendFSMEvent()
     {
-        FSMToSendEventTo.SendEvent("PathComplete");
-        GoToNextLocation();
+        StartCoroutine(GoToNextLocation());
     }
 
     // Update is called once per frame
-    public void GoToNextLocation()
+    public IEnumerator GoToNextLocation()
     {
+        yield return new WaitForSeconds(pauseDurationAfterReachDestination);
         currentLoc++;
         if (currentLoc >= Locations.Count)
         {
